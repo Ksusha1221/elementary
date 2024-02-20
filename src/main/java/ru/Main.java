69b4f02490bb25d932e1d489ebf1b0400d4fd8c9
Ксения <priaha1996@gmail.com>
2024-02-20 15:03:35 +0300

    1.8

--- src/main/java/ru/Main.java
+++ src/main/java/ru/Main.java
@@ -1,7 +1,9 @@
 package ru;
 
+import ru.calculator.MathFunc;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println(MathFunc.func1(3));
     }
 }
