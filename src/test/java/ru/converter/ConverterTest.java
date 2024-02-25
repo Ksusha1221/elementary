debecf683b3271978a572c4888289b47026e2e99
Ксения <priaha1996@gmail.com>
2024-02-25 12:34:15 +0300

    2.1 Модульные тесты

---
+++ src/test/java/ru/converter/ConverterTest.java
@@ -0,0 +1,26 @@
+package ru.converter;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class ConverterTest {
+
+        @Test
+        void whenConvert140RblThen1Dot4Euro() {
+            float in = 140;
+            float expected = 1.4f;
+            float out = Converter.rubleToEuro(in);
+            float eps = 0.0001f;
+            assertThat(out).isEqualTo(expected, withPrecision(eps));
+        }
+
+        @Test
+        void whenConvert200RblThen2dot2222Dlr() { //доделать
+            float in1 = 90;
+            float expected1 = 1.0f;
+            float out1 = Converter.rubleToDollar(in1);
+            float eps1 = 0.0001f;
+            assertThat(out1).isEqualTo(expected1, withPrecision(eps1));
+        }
+
+}
