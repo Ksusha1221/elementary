1635f49949d9d118a7ecfa4dde6790a26d7f43bc
Ксения <priaha1996@gmail.com>
2024-02-25 14:44:27 +0300

    2.6

---
+++ src/test/java/ru/condition/SqAreaTest.java
@@ -0,0 +1,34 @@
+package ru.condition;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class SqAreaTest {
+
+    @Test
+    void whenP6K2Square2() {
+        int expected = 2;
+        int p = 6;
+        double k = 2;
+        double out = SqArea.square(p, k);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void whenP7K2Square2Dot29() {
+        double expected = 2.29;
+        int p = 7;
+        double k = 3;
+        double out = SqArea.square(p, k);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void whenP5K2Square4() {
+        int expected = 1;
+        int p = 5;
+        double k = 4;
+        double out = SqArea.square(p, k);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+}
