2f8f75135867589b4d9d3b1fcb8e556ac813cfe5
Ксения <priaha1996@gmail.com>
2024-03-02 18:12:48 +0300

    4.5 Создать программу вычисляющую факториал

--- src/main/java/ru/loop/Factorial.java
+++ src/main/java/ru/loop/Factorial.java
@@ -1,16 +1,10 @@
 package ru.loop;
 
 public class Factorial {
-    @SuppressWarnings("checkstyle:WhitespaceAround")
     public static int calc(int n) {
-        int rsl = 0;
-        if (n == 0 || n == 1) {
-            rsl = 1;
-        } else if (n > 0){
-            rsl = 1;
-            for (int i = 1; i <= n; i++) {
-                rsl *= i;
-            }
+        int rsl = 1;
+        for (int i = 1; i <= n; i++) {
+            rsl *= i;
         }
         return rsl;
     }
