a09cb381abe7779d5dc7cabd52ca26ea43b85923
Ксения <priaha1996@gmail.com>
2024-03-12 16:47:26 +0300

    5.15 Сравнить последние элементы двух массивов

---
+++ src/main/java/ru/array/RollBackArray.java
@@ -0,0 +1,12 @@
+package ru.array;
+
+public class RollBackArray {
+    public static int[] rollback(int[] array) {
+        int[] result = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            result[array.length - 1 - i] = array[i];
+        }
+        return result;
+    }
+}
+
