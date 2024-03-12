13e96960a630698e4d8613da36fd99355367e8ea
Ксения <priaha1996@gmail.com>
2024-03-12 11:36:08 +0300

    4.14 Генерация конструкций в IDEA

--- src/main/java/ru/array/SwitchArray.java
+++ src/main/java/ru/array/SwitchArray.java
@@ -9,7 +9,15 @@ public class SwitchArray {
         return array;
     }
 
-    public static void main(String[] args) {
+    public static int[] swap(int[] array, int source, int dest) {
+        int t = array[source];
+        array[source] = array[dest];
+        array[dest] = t;
+        return array;
+    }
+
+
+        public static void main(String[] args) {
         int[] nums = new int[] {1, 2, 3, 4, 5, 6};
         int[] rsl = swapBorder(nums);
         for (int index = 0; index < rsl.length; index++) {
