7643e785b443c1f51f188179e7250827c5777fee
Ксения <priaha1996@gmail.com>
2024-03-20 13:05:10 +0300

    5.29 Массив из диагонали матрицы

---
+++ src/main/java/ru/array/TwoNumberSum.java
@@ -0,0 +1,11 @@
+package ru.array;
+
+public class TwoNumberSum {
+    public static int[] getIndexes(int[] array, int target) {
+        int i = 0;
+        int j = array.length - 1;
+        while (i < j)
+        return new int[0];
+    }
+}
+
